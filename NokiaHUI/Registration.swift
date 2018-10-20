ece8ddcca1710141a763670c7f7d4dcc3b54ced5
Mikhail <wowivan2011@yandex.ru>
2018-10-20 23:11:50 +0200

    Add Sex position in Registration page

--- NokiaHUI/Registration.swift
+++ NokiaHUI/Registration.swift
@@ -9,13 +9,34 @@
 import UIKit
 import Alamofire
 
-class Registration : UIViewController {
+class Registration : UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return sex.count
+    }
+    
     
     @IBOutlet weak var Data: UIDatePicker!
+    @IBOutlet weak var sexPicker: UIPickerView!
+    let sex = ["Male", "Female"]
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         Data.backgroundColor = UIColor.white
+        
+        sexPicker.delegate = self
+        sexPicker.dataSource = self
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return sex[row]
+    }
+    
+    @IBAction func SingUpButton(_ sender: Any) {
+        
     }
     
 }
